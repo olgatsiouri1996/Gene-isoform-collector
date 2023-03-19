8a75b026d2cafa10b6da530fda5eb45d71a169f3
Olga Tsiouri <olgatsiouri@outlook.com>
2023-03-20 01:43:30 +0200

    .

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gene isoform collector
+Windows and linux GUI and command line app that searches and exports the isoforms of each input gene loci
+![](img/program_gui.png)
+## Executables
+executables can be found at [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.7750822.svg)](https://doi.org/10.5281/zenodo.7750822) 
+## Citation
+To cite this app cite the following:
+1. Olga Tsiouri. (2023). Gene isoform collector: Windows and linux GUI and command line app that searches and exports the isoforms of each input gene loci. Zenodo. https://doi.org/10.5281/zenodo.7750822
+2. Chris. (2023). Gooey [Python]. Retrieved from https://github.com/chriskiehl/Gooey (Original work published 2014)
+3. Shirley, M. D., Ma, Z., Pedersen, B. S., & Wheelan, S. J. (2015). Efficient ‘pythonic’ access to FASTA files using pyfaidx (No. e1196). PeerJ Inc. [doi: 10.7287/peerj.preprints.970v1](https://peerj.com/preprints/970v1/) 
+## Example data
+The imput example data in `example/` are from [lotus base](https://lotus.au.dk/)
---
+++ src/GUI/Gene isoforms collector.py
@@ -0,0 +1,67 @@
+import os
+from pyfaidx import Fasta
+from gooey import Gooey, GooeyParser
+
+# add gooey decorator
+@Gooey(required_cols=3, program_name='Gene isoforms collector', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+def main():
+    # Create the parser object
+    ap = GooeyParser(description='Searches and exports the isoforms of each input gene loci')
+
+    # Add arguments for the input files
+    ap.add_argument('-loci','--loci file', required=True, widget='FileChooser', help='1-column text file containing gene loci')
+    ap.add_argument('-input','--input fasta file', required=True, widget='FileChooser', help='input multi-FASTA file')
+    ap.add_argument('-dir','--output directory', required=True, widget='DirChooser', help='Directory to save fasta files')
+
+    # Add optional arguments
+    ap.add_argument('-name','--multifasta filename',type=str,required=False, help='output single file name')
+    ap.add_argument('-type','--type', required=False, type=str, widget='Dropdown', default='single file', choices=['single file','1 file per gene loci','1 file per gene isoform'], help="output type to choose")
+
+    # Parse the arguments
+    args = vars(ap.parse_args())
+ 
+   # Helper function to wrap fasta sequence to 60 characters per line
+    def wrap_fasta_seq(seq):
+        return '\n'.join([seq[i:i+60] for i in range(0, len(seq), 60)])
+
+
+    # Read in the FASTA file with long names
+    fasta = Fasta(args['input fasta file'], read_long_names=True)
+
+    # Read in the gene loci file
+    with open(args['loci file'], 'r') as f:
+        loci_list = [line.rstrip() for line in f]
+
+    # Initialize a dictionary to store the sequences
+    seq_dict = {}
+
+    # Loop through the loci and search for matches in the FASTA file
+    for key in fasta.keys():
+        for locus in loci_list:
+            if locus in key:
+                # If the locus is found, add it to the dictionary
+                seq_dict[key.rstrip()] = wrap_fasta_seq(fasta[key][:].seq)
+
+    # Choose directory to save output files
+    os.chdir(args['output directory'])
+
+    # Write the output
+    if args['type'] == 'single file':
+        # Output all sequences in a single file
+        with open(args['multifasta filename']+'.fasta', 'w') as f:
+            for locus, seq in seq_dict.items():
+                f.write(f'>{locus}\n{seq}\n')
+    elif args['type'] == '1 file per gene loci':
+        # Create separate files 1 for each input loci
+        for loci in loci_list:
+            with open(loci+'.fasta', 'w') as f:
+                for locus, seq in seq_dict.items():
+                        if loci in locus:
+                            f.write(f'>{locus}\n{seq}\n')
+    else:
+        for locus, seq in seq_dict.items(): # Create separate files 1 for each gene isoform
+            with open(locus.split(' ')[0]+'.fasta', 'w') as f:
+                f.write(f'>{locus}\n{seq}\n')
+
+if __name__ == '__main__':
+    main()
